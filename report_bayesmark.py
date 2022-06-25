2734000632ed5ef71b83ca39490be0043cbc2418
xadrianzetx <xadrianzetx@gmail.com>
2022-06-25 17:26:36 +0200

    Offload report rendering to template engine

--- report_bayesmark.py
+++ report_bayesmark.py
@@ -8,6 +8,7 @@ from typing import Dict, Generator, List, Tuple
 
 import numpy as np
 import pandas as pd
+from jinja2 import Environment, FileSystemLoader
 from scipy.special import binom
 from scipy.stats import mannwhitneyu
 
@@ -247,31 +248,10 @@ class BayesmarkReportBuilder:
 
     def assemble_report(self) -> str:
 
-        num_datasets = len(self._datasets)
-        num_models = len(self._models)
-
-        overall_body = io.StringIO()
-        overall_body.write(_OVERALL_HEADER)
-        for solver in self._solvers:
-            row = f"|{solver}|{self._borda[solver]}|{self._firsts[solver]}|"
-            overall_body.write("".join([row, _LINE_BREAK]))
-
-        with open("report_template.md") as file:
-            report_template = file.read()
-
-        report = report_template.format(
-            num_solvers=len(self._solvers),
-            num_datasets=num_datasets,
-            num_models=num_models,
-            precedence=self._metric_precedence,
-            num_problems=num_datasets * num_models,
-            overall=overall_body.getvalue(),
-            leaderboards=self._problems_body.getvalue(),
-        )
-
-        overall_body.close()
-        self._problems_body.close()
-        return report
+        loader = FileSystemLoader(".")
+        env = Environment(loader=loader)
+        report_template = env.get_template("report_template.md")
+        return report_template.render(report=self)
 
 
 def build_report() -> None:
--- report_template.md
+++ report_template.md
@@ -1,10 +1,10 @@
 # Benchmark Result Report
 
-* Number of Solvers: {num_solvers}
-* Number of Models: {num_models}
-* Number of Datasets: {num_datasets}
-* Number of Problems: {num_problems}
-* Metrics Precedence: `{precedence}`
+* Number of Solvers: {{ report._solvers|length }}
+* Number of Models: {{ report._models|length }}
+* Number of Datasets: {{ report._datasets|length }}
+* Number of Problems: {{ report._problems|length }}
+* Metrics Precedence: {{ report._metric_precedence }}
 
 Please refer to ["A Strategy for Ranking Optimizers using Multiple Criteria"][Dewancker, Ian, et al., 2016] for the ranking strategy used in this report.
 
@@ -19,11 +19,23 @@ Please refer to ["A Strategy for Ranking Optimizers using Multiple Criteria"][De
 
 ## Overall Results
 
-{overall}
+|Solver|Borda|Firsts|
+|:---|---:|---:|
+{% for solver in report._solvers -%}
+|{{ solver }}|{{ report._borda[solver] }}|{{ report._firsts[solver] }}|
+{% endfor %}
 
 ## Problem Leaderboards
 
-{leaderboards}
+{% for problem in report._problems %}
+### ({{ problem.number }}) Problem: {{ problem.name }}
+
+|Ranking|Solver|{%- for metric in problem.metrics -%}{{ metric.name }} (avg +- std)|{% endfor %}
+|:---|---:|{%- for _ in range(problem.metrics|length) -%}---:|{% endfor %}
+{% for row in problem.rows -%}
+|{{ row.pos }}|{{ row.solver }}|{%- for moments in row.moments -%}{{ moments[0] }} +- {{ moments[1] }}|{% endfor %}
+{% endfor -%}
+{% endfor %}
 
 ## Datasets
 
